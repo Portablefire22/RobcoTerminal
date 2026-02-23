e94b37f5e5e296d4e9492ccc8e75b53ceafb8faf
Portablefire22 <lilith@kitten.rs>
2026-02-23 19:27:15 +0000

    Added main menu with dynamic button array. Added a dynamically sized button array that can change state or open urls

---
+++ UOS/UOS.Client/Types/RobcoButtonEntry.cs
@@ -0,0 +1,27 @@
+﻿namespace UOS.Client.Types;
+
+public class RobcoButtonEntry
+{
+    public string Text { get; set; } = ">!INVALID!<";
+    public bool FinishedRendering { get; set; } = false;
+    public int State { get; set; } = -1;
+    public string? Url { get; set; } = null;
+    public bool Selected { get; set; }
+    public string Class => Selected ? "robco-hovered" : "";
+
+    public RobcoButtonEntry()
+    {
+    }
+
+    public RobcoButtonEntry(string text, int state)
+    {
+        Text = text;
+        State = state;
+    }
+
+    public RobcoButtonEntry(string text, string? url)
+    {
+        Text = text;
+        Url = url;
+    }
+}
